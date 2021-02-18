c36f6b1c00972cb0e43062b4aeb0877e01d6fc04
Aziz <68072877+AbdurakhimovAziz@users.noreply.github.com>
2021-02-18 11:36:47 +0500

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+exports.min = function min(array) {
+    return array === undefined
+        ? 0
+        : array.reduce((min, el) => (el < min ? el : min), 0);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    return array === undefined
+        ? 0
+        : array.reduce((max, el) => (el > max ? el : max), 0);
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    return array === undefined || array.length === 0
+        ? 0
+        : array.reduce((sum, el) => sum + el) / array.length;
+};
